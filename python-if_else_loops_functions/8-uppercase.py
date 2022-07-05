e4e340640899b678d296c0ade0e5189776ba09f3
meekmarcelin <meekmarcelin@gmail.com>
2022-07-05 10:53:37 -0700

    uppercase commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
+def uppercase(str1):
+    for i in str1:
         i = ord(i)
         if 97 <= i <= 122:
             i = i - 32
             print("{:c}".format(i), end='')
-        print("")
+    print("")
