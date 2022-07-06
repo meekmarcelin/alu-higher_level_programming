4156bdb4b6906898b195b2c418b1a5eecfa3d0e3
meekmarcelin <meekmarcelin@gmail.com>
2022-07-06 02:36:46 -0700

    args commit

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name == "__main__":
+    import sys
+values = sys.argv
+number = len9(values)
+i = 1
+if number == 1:
+    print("0 arguments.")
+elif number == 2:
+    print("1 argument:")
+    print("1: {}".format(values[1]))
+elif number > 2:
+    print("{:d} arguments:".format(number-1))
+    while i < number:
+        print("{d}: {}".format((i), values[i]))
+        i = i + 1
