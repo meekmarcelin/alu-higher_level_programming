ae07749dc49d9a4bbb0d2233d06959987c99bfe1
meekmarcelin <meekmarcelin@gmail.com>
2022-07-11 11:44:13 -0700

    no commit

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     my_string_list = list(my_string)
     index_count = 0
-    for letter in my_string_list:
+    for index in my_string_list:
         if index  == 'c' or index == 'c':
             my_string_list[index_count] = ""
         index_count += 1
