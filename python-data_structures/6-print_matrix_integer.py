61fda5081bca3e9e4f946fc735ac1bf3936ef943
meekmarcelin <meekmarcelin@gmail.com>
2022-07-12 04:45:13 -0700

    matrix commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for record in matrix:
-        count = 0
+        counter = 0
         for i in record:
             if counter == (len(record) - 1):
                 print("{:d}".format(i), end="")
