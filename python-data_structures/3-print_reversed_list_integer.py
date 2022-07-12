6a1433836309e40d82a2c0d11e474d9ba8ea5fe6
meekmarcelin <meekmarcelin@gmail.com>
2022-07-12 03:55:08 -0700

    reversed commit

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if integer(my_list, list):
-        count = len(my_list) -1
+        count = len(my_list) - 1
         if count >= 0:
             for i in my_list:
                 print("{:d}".format(my_list[count]))
