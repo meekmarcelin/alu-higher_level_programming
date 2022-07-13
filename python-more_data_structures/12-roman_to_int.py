bcca32d400a7cc0b943ac2d1e09a037672c1bc42
meekmarcelin <meekmarcelin@gmail.com>
2022-07-13 12:25:59 -0700

    roman commit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
-        return None
+        return 0
     summ = 0
     number = 0
     given = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
