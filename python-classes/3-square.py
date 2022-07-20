440fedfad7083aa529eddbd9b2ccdfc1907cedf9
meekmarcelin <meekmarcelin@gmail.com>
2022-07-20 07:48:23 -0700

    3 commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
 """
-3-square.py
+NO MODULE USED
 """
 
 
-
 class Square:
-    """DEFINE A SQUARE""
-
+    """
+    SQUARE CLASS
+    """
     def __init__(self, size=0):
-        """
-        creates an instance of square
-        Args:
-            size: size of the square
-        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-
-        self.__size = size
+        else:
+            self._Square__size = size
 
     def area(self):
-        """
-        Finds area of the square
-        """
-        return self.__size * self.__size
+        return self._Square__size**2
 
 
 
