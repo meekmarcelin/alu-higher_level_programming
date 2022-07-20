5a1417fb51716f5e9eb0396df8190bfa2f976ed7
meekmarcelin <meekmarcelin@gmail.com>
2022-07-20 04:26:17 -0700

    2 commit

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+DOCUMENTATION MUDULE
+"""
+
+
+class Square:
+    """
+    DIFINE THE SQUARE SIZE
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            riase TypeError("size is integer")
+        elif size < 0:
+            riase ValueError("size >=0")
+        else:
+            self._Square__size = size
