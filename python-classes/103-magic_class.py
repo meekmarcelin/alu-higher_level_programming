3b568311caa9199c4cdd8c6a025aefac9ca24a17
meekmarcelin <meekmarcelin@gmail.com>
2022-07-22 04:50:13 -0700

    magic commit

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Do some magic math"""
+import math
+""" Do some magic math""
+
+
+class MagicClass:
+    """make a circle instance"""
+    def __init__(self, radius=0):
+        if type(radius) != int and type(radius) != float:
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
+
+    def area(self):
+        return (self._MagicClass__radius ** 2) * math.pi
+
+    def circumference(self):
+        return (2 * math.pi * self._MagicClass__radius)
+
