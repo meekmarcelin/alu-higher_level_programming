f54967190bb8740dd656979dea643c20de065963
meekmarcelin <meekmarcelin@gmail.com>
2022-07-26 05:33:50 -0700

    101 commit

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"""
-LockedClass
-"""
+""" Locked Class"""
 
 
-class LockedClass
-    """No class or object attribut, Except for first_time
-    """
-    def __setattr__(self, attribut, value):
-        if attribute == "first_name":
-            self.__dict__[attribute] = value
-        else:
-            raise AttributeError("'LockedCladd' object has no attribut'" + attribute + "'")
+class LockedClass:
+    """using low memory cost"""
+    __slots__ = "first_name"
