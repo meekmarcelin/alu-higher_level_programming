dcfd3ea1f79c9d7e22c48f3eeed4fd2a529ec302
meekmarcelin <meekmarcelin@gmail.com>
2022-07-27 03:45:43 -0700

    7 commit

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" base geometry """
+
+
+class BaseGeometry:
+    """ Geometry """
+
+    def area(self):
+        """ raise exception """
+        raise Exceprtion("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ give a value """
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
