fe65ae8784ef196ccdc20fe7ac1ee3139f16cfb4
meekmarcelin <meekmarcelin@gmail.com>
2022-07-27 03:02:44 -0700

    3 commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Kind of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ function returns true if obj is isinstance else false """
+    return isinstance(obj, a_class)
