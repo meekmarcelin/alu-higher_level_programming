c8a4e93505ae3eb89c825a18575ae278a98630d9
meekmarcelin <meekmarcelin@gmail.com>
2022-07-28 13:16:25 -0700

    5 commit

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd "email=hr@m.sibomana@alustudent.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -sd "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
