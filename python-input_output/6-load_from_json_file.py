10d84794f5af6d86259b4d6b8505c687a032bb06
meekmarcelin <meekmarcelin@gmail.com>
2022-07-28 04:22:46 -0700

    6 commit

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" json module for reading """
+import json
+""" do both reading and loading """
+
+
+def load_from_json_file(filename):
+    """load json and read it as well """
+    with open(filename, "r" encoding="utf-8") as f:
+        return json.loads(f.read())
