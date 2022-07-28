620d8adb1ce6ea2a1e4aed836216670cb470696a
meekmarcelin <meekmarcelin@gmail.com>
2022-07-28 05:16:12 -0700

    11 commit

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""Student
-"""
+""" json to student """
 
 
 class Student:
-    """Contains student data
-    """
+    """ first  student class """
 
     def __init__(self, first_name, last_name, age):
+        """ first statement for the class """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves dictionary of Student with conditions to filter
-        """
+        """ function for converting """
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__]
 
-        if attrs == None or type(attrs) != list:
-            return self.__dict__
-        else:
-            temp = {}
-            for elem in attrs:
-                if type(elem) != str:
-                    return self.__dict__
-                if elem in self.__dict__.keys():
-                    temp[elem] = self.__dict__[elem]
-            return temp
+    def reload_from_json(sel, json):
+        """ a function for reloading""
 
-    def reload_from_json(self, json):
-        """Replaces all items in `json`
-        """
 
-        for items in json.keys():
-            self.__dict__[items] = json[items]
+        for i, j in json.items():
+            self.__dict__[i] = j
