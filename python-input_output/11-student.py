748a1bff011e3223a9ce42cf336f4204957bab53
meekmarcelin <meekmarcelin@gmail.com>
2022-07-28 05:20:32 -0700

    11 commit

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" json to student """
+"""
+A Student class that defines a Studen (module)
+"""
 
 
-class Student:
-    """ first  student class """
-
+class Student():
+    """
+        A Student class that defines a Student
+    """
     def __init__(self, first_name, last_name, age):
-        """ first statement for the class """
+        """
+            INIT
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ function for converting """
+        """
+            retrieves a dictionary representation
+            of a Student instance
+        """
         if type(attrs) is list and all([type(x) == str for x in attrs]):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
-        return self.__dict__]
+        return(self.__dict__)
 
-    def reload_from_json(sel, json):
-      """
+    def reload_from_json(self, json):
+        """
         """
         for i, j in json.items():
             self.__dict__[i] = j
