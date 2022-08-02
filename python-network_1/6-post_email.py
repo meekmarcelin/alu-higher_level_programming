09e8d48e598b088e16de13b1136eaabb3bc1b788
meekmarcelin <meekmarcelin@gmail.com>
2022-08-02 11:37:22 -0700

    6 commit

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     test = {'email': sys.argv[2]}
-    r = request.post(url, data=test)
+    r = requests.post(url, data=test)
     print(r.text)
