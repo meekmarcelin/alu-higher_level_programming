1928a55693f5bc5676b6acb536976f38f945336c
meekmarcelin <meekmarcelin@gmail.com>
2022-08-02 07:19:50 -0700

    0 commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""' FETCHES """
+"""
+Fetches
+"""
+import urllib.request
 
 
-def fetcher():
-    """ Fetcher """
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") aas response:
-    html = response.read()
-    print("Body response")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
-    if __name__ == "__main__":
-        fetcher()
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
+        body = reply.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
