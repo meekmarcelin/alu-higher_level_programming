fed76c87217717f3f61ebed8319e5ff09cab415e
meekmarcelin <meekmarcelin@gmail.com>
2022-08-02 11:19:38 -0700

    5 commit

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Send a request """
+import requests
+import sys
+
+
+if __name == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers.get('x-request-id'))
