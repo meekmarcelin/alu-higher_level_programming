f643c88a03d207cb7af343176e847d41386c7e67
meekmarcelin <meekmarcelin@gmail.com>
2022-08-02 11:47:48 -0700

    7 commit

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -4,16 +4,11 @@ import requests
 import sys
 
 
-def errorcode():
-    """status"""
-    result = requests.get(sys.argv[1])
-    try:
-        if result.status_code > 400:
-            print("Error code: {}".format(result.status_code))
-        else:
-            print(result.content.decode("utf-8"))
-    except KeyError:
-        pass
-
 if __name__ == "__main__":
-    errorcode()
+    url = sys.argv[1]
+    r = requests.get(url)
+    try:
+        r.raise_for_status()
+        print(r.text)
+    except Exception as e:
+        print("Error code: {}".format(r.status_code))
