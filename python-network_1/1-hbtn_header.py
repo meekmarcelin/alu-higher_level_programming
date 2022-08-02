8f1e06375f6a5b0aa3af85b9b204e475a80a64d4
meekmarcelin <meekmarcelin@gmail.com>
2022-08-02 09:58:52 -0700

    1 commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,5 +5,5 @@ import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.arg[1]) as response:
-        print(response.info()['X-Request_Id'])
+    with urllib.request.urlopen(sys.arg[1]) as res:
+        print(res.info()['X-Request_Id'])
