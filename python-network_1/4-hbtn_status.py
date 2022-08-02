1de5a8d4a989ff06086f473ecce7605f806d45dd
meekmarcelin <meekmarcelin@gmail.com>
2022-08-02 10:44:58 -0700

    4 commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ import requests
 
 if __name__ == "__main__":
     reply = requests.get('https://intranet.hbtn.io/status').text
-    print("Body response")
+    print("Body response:")
     print("\t- type: {}".format(type(reply)))
     print("\t- content: {}".format(reply))
