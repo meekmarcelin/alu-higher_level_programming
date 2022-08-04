4c1beab2f38c320c876b664c5946e7c896a8b033
meekmarcelin <meekmarcelin@gmail.com>
2022-08-04 10:42:46 -0700

    5 commit

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- create the table
-CREATE TABLE IF NOT EXISTS uniqu_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
