709647457dfa4218984b14d2a8663a14d8c87200
meekmarcelin <meekmarcelin@gmail.com>
2022-08-05 02:36:12 -0700

    11 commit

---
+++ SQL_more_queries/11-genre_id_all_show.sql
@@ -0,0 +1,5 @@
+-- import database
+SELECT tv_shows.title tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.tittle ASC, tv_show_genres.genre_id ASC;
