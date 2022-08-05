05b2b92a332ca2d67d982f190fc5bc0aae71eed3
meekmarcelin <meekmarcelin@gmail.com>
2022-08-05 03:57:51 -0700

    15 commit

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -3,5 +3,5 @@ SELECT x.title
 FROM tv_shows x
 JOIN tv_show_genres y ON x.id = y.show_id
 JOIN tv_genres s ON y.genre_id = s.id
-WHERE s.name = 'comedy'
-ORDER BY a.title ASC;
+WHERE s.name = 'Comedy'
+ORDER BY x.title ASC;
