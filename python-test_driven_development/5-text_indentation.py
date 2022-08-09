e9d218c76390d6bd781be349e6ea7bf07800cb2d
meekmarcelin <meekmarcelin@gmail.com>
2022-08-09 10:21:19 -0700

    5 commit

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" print a text """
+
+
+def text_indentation(text):
+      """ text indentation """
+    if type(text) != str:
+        raise TypeError('text must be a string')
+    sentence = (':' + '\n\n').join([x.strip(" ") for x in text.split(':')])
+    sentence = ('.' + '\n\n').join([y.strip(" ") for y in sentence.split('.')])
+    sentence = ('?' + '\n\n').join([z.strip(" ") for z in sentence.split('?')])
+    print(sentence, end="")
