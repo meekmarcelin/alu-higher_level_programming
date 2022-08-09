b0e1688c972336e907a42353e66b20ff3617221d
meekmarcelin <meekmarcelin@gmail.com>
2022-08-09 10:41:09 -0700

    6 commit

---
+++ python-test_driven_development/6-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" write unittests """
+
+
+def max_integer(list=[]):
+    """ test file of tests """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
