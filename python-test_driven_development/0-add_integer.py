be0f7ccf6933bb006d88224f66a5ac421508b532
meekmarcelin <meekmarcelin@gmail.com>
2022-08-09 08:02:43 -0700

    0 commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""
-add_integer:
-    Checks if parameters are int
-    Returns sum of parameters
-"""
+"""addition function module"""
 
 
 def add_integer(a, b=98):
-    """
-    Checks if int, otherwise return sum
-    """
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
+    """add two number together"""
+    if type(a) != int and type(a) != float:
+        raise TypeError('a must be an integer')
+    elif type(b) != int and type(b) != float:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
 
-    if type(a) != int:
-        raise TypeError("a must be an integer")
-    elif type(b) != int:
-        raise TypeError("b must be an integer")
-    else:
-        return a + b
