6df2494a24103eb73152cb837df140ab1a342429
meekmarcelin <meekmarcelin@gmail.com>
2022-08-09 09:53:50 -0700

    4 commit

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" print only squares """
+
+
+def print_square(size):
+    """ print squares """
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    if size == 0:
+        return
+    else:
+        s = 0
+        while i < size:
+            print('#' * size)
+            s += 1
