5123004ae4715d8b4c29afb3094679c4b72c574f
meekmarcelin <meekmarcelin@gmail.com>
2022-08-09 07:47:08 -0700

    0 commit

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" add integers """
+
+
+def add_integer(a, b=98):
+    """ add numbers """
+    if type(a) != int and type(a) != float:
+        raise TypeError('a must be an integer')
+    elif type(b) != int and type(b) != float:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
