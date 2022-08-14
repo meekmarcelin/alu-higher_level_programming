fcd70deef63f018ef81a260336efd83dfc0d241c
meekmarcelin <meekmarcelin@gmail.com>
2022-08-14 10:45:53 -0700

    base commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,9 +15,19 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    
     @staticmethod
     def to_json_string(list_dictionaries):
         """return json representation"""
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save json to file"""
+        dict_list = None
+        if list_objs is not None:
+            dict_list = [i.to_dictionary() for i in list_objs]
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            f.write(Base.to_json_string(dict_list))
