25a8749a801d5f75ae80f829e308d611271d320a
meekmarcelin <meekmarcelin@gmail.com>
2022-08-14 10:06:00 -0700

    base commit

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+""" Create a class """
+import json
+
+
+class Base:
+    """ create a real base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ first base function """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
