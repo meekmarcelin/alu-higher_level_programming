7f2ed13d3e0fd311874872eadea78656cf0c1837
meekmarcelin <meekmarcelin@gmail.com>
2022-08-14 10:34:46 -0700

    base commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ list all dictionaries """
-        if list_dictionaries is None: 
+        if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
