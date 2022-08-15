415a6c8e009476be64d42e42df2d05c27624c990
meekmarcelin <meekmarcelin@gmail.com>
2022-08-15 07:36:17 -0700

    square commit

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""" create a class square """
+"""square
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class for square """
+    """Inherits from Rectangle
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ function for square """
-        super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """ attribute for square """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
