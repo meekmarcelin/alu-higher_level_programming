27417728403fc06cdc52f6ebc39b9c2631fd6d23
meekmarcelin <meekmarcelin@gmail.com>
2022-08-15 07:42:46 -0700

    square commit

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,53 +1,15 @@
 #!/usr/bin/python3
-"""square
-"""
+"""Square class module"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from Rectangle
-    """
+    """defines square instances"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(width=size, height=size, x=x, y=y, id=id)
-
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """size needs to be an int
-        """
-
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        """Returns formatted information display
-        """
-
-        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
-                                             self.id, self.x, self.y,
-                                             self.width)
-
-    def update(self, *args, **kwargs):
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-        elif len(args) != 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[2]
-                self.y = args[3]
-            except IndexError:
-                pass
-        else:
-            print()
+        """instantiate a square"""
+        super().__init__(size, size, x, y, id)
 
     def to_dictionary(self):
-        """Returns a dict representation
-        """
-
-        return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y})
+        """return square dictionary representation"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
