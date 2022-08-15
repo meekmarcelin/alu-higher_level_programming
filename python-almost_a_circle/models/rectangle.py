d84b4cf6d0646aab5c59a15f02df7559e4571c1a
meekmarcelin <meekmarcelin@gmail.com>
2022-08-15 03:21:01 -0700

    rectangle commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
+"""rectangle
 """
-class Rectangle
-"""
-from .base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
-    """class"""
+    """Inherits from Base
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ class constructor"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
