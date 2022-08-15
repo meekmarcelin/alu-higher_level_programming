952278d68e5b112331fea758db8c422ea3fc2b90
meekmarcelin <meekmarcelin@gmail.com>
2022-08-15 10:10:03 -0700

    base commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ json must be returned """
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
