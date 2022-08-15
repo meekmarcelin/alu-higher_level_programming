592f2e7112c4af0ad03274b1e099ccb39cc1e598
meekmarcelin <meekmarcelin@gmail.com>
2022-08-15 03:10:31 -0700

    rectangle commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" rectangle class """
+"""create rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """ class for rectangle """
+    """class for  rectangles"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ rectangle function """
+        """function for rectangle"""
         super().__init__(id)
         self.width = width
         self.height = height
