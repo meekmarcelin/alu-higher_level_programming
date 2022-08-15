910c3ae791b5b3b2b66605b5446bb6b375ef96fd
meekmarcelin <meekmarcelin@gmail.com>
2022-08-15 08:45:25 -0700

    square commit

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,24 +1,52 @@
 #!/usr/bin/python3
-""" create a class for python """
+"""
+class Square
+"""
 from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ square class """
+    """docstring for ."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ function for square """
+        """ class constructor"""
         super().__init__(size, size, x, y, id)
 
-    def to_dictionary(self):
-        """ dic to return square """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+    def __str__(self):
+        """Defines print attribute """
+        attributes = "[Square] ({}) {}/{} - {}"
+        return attributes.format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
+        """ Getter of the size attribute """
         return self.width
 
     @size.setter
     def size(self, value):
+        """ setter of the size attribute """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """method that assigns attributes"""
+        if len(args):
+            i = 0
+            keys_of_attributes = ['id', 'size', 'x', 'y']
+            for value in args:
+                if i < 4:
+                    setattr(self, keys_of_attributes[i], value)
+                    i += 1
+        else:
+            for keys_of_attributes, value in kwargs.items():
+                setattr(self, keys_of_attributes, value)
+
+    def to_dictionary(self):
+        """ Returns a square dictionary """
+        dic = {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+            }
+        return dic
