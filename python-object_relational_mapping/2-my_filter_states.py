1ae91bf27e25549e446a01349a984a0353c5487c
meekmarcelin <meekmarcelin@gmail.com>
2022-08-22 11:17:57 -0700

    2 commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ Display all values in states """
-from sys import argv
-import MySQLdb
 
 
 if __name__ == "__main__":
-     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = '{}'\
     ORDER BY states.id ASC".format(argv[4]))
     lst = cur.fetchall()
-    for r in lst:
+    for s in lst:
         if r[1] == argv[4]:
             print(r)
     cur.close()
