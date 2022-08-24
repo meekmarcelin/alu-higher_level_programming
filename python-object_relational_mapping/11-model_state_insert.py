4752b27e748394c51437428f97127e654dc5af68
meekmarcelin <meekmarcelin@gmail.com>
2022-08-24 11:23:58 -0700

    11 comit

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-""" Add state called Lousiana """
+"""Add the State Louisiana
+"""
 import sys
 from sqlalchemy.orm import session
 from sqlalchemy import create_engine
 from model_state import Base, State
 
 if __name__ == "__main__":
-    ingine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = session(engine)
-    state = State(name='Louisiana')
+    session = Session()
 
+    state = State(name='Louisiana')
     session.add(state)
     session.commit()
-    print(stae.id)
+
+    print(state.id)
