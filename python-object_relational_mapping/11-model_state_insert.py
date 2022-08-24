b00ed6af2a231296f1472192a97c1e464c2fcd19
meekmarcelin <meekmarcelin@gmail.com>
2022-08-24 11:19:06 -0700

    11 comit

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -7,7 +7,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     ingine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = session(engine)
     state = State(name='Louisiana')
 
