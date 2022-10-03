ac0593426cd9b3bfaaf382e050bbf5b60a6daace
meekmarcelin <meekmarcelin@gmail.com>
2022-10-03 11:13:52 -0700

    3 commit

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2]) { console.log(process.argv[1]); } else { console.log('No argument'); }
+if (process.argv[2]) { console.log(process.argv[2]); } else { console.log('No argument'); }
