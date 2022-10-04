ee666d5e012ef7d157b8332ab046cd68def1da2d
meekmarcelin <meekmarcelin@gmail.com>
2022-10-04 04:04:08 -0700

    7 commit

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const arg = parseInt(process.argv[2]);
 if (!arg) { console.log('Missing number of occurences'); }
-for (let i = 0; i < arg.length; i++); { console.log('C is fun') }
+for (let i = 0; i < arg; i++); { console.log('C is fun') }
