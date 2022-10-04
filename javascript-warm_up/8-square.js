20a1a2ac2410d53f4a901046b48008ccda4ce26f
meekmarcelin <meekmarcelin@gmail.com>
2022-10-04 09:06:47 -0700

    8 commit

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node 
 const arg = parseInt(process.argv[2]);
 if (!arg) { console.log('Missing size'); }
-for (let i = 0; i < arg; i++) { console.log('X'.repeat(arg)); }
+for (let i = 0; i < arg; i++) { console.log('X'.repeat(arg)); } 
