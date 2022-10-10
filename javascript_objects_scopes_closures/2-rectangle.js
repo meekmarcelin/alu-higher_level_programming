761b3cc790c7c2e8a5d67e4925ac394a46972930
meekmarcelin <meekmarcelin@gmail.com>
2022-10-10 12:22:03 -0700

    2 commit

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-module.exports = class Rectangle { constructor (w, h) { if (w > 0 && h > 0) { this.width = w, this.height = h; } } };
+module.exports = class Rectangle { constructor (w, h) { if (w > 0 && h > 0) { this.width = w; this.height = h; } } };
