1e2198b07be6a59584d6926ed15090adbaa3f82c
meekmarcelin <meekmarcelin@gmail.com>
2022-10-10 13:13:43 -0700

    4 commit

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,2 +1,26 @@
 #!/usr/bin/node
-module.exports = class Rectangle { constructor(w, h) { this.width = w; this.height = h; { if w > 0 && h > 0) } } print () { for (let i = 0; i < this.height; i++) { console.log('X'.repeat(this.width)); } } rotate () { this.width = [this.height, this.height = this.width][0];} double () { this.width *=2; this.height *= 2; } };
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
