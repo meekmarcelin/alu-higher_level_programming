645d9637ab0233a5c6cdc7b17779d8f8b1dba04a
meekmarcelin <meekmarcelin@gmail.com>
2022-10-11 07:00:07 -0700

    2 commit

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-require('request').get(process.argv[2], function (err, r) { if (err) { console.log(err); } else { console.log('code: ' + r.statuscode); } });
+require('request').get(process.argv[2], function (err, r) { if (err) { console.log(err); } else { console.log('code: ' + r.statusCode); } });
