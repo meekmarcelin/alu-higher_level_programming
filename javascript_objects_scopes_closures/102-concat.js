2dfd1f05ea5860b9996cb33741458efcf6648164
meekmarcelin <meekmarcelin@gmail.com>
2022-10-11 02:28:03 -0700

    102 commit

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-let text1 = fs.readFileSync(process.argv[2], 'utf-8');
-let text2 = fs.readFileSync(process.argv[3], 'utf-8');
-fs.appendFile(process.argv[4], text1.concat(text2).trim());
-fs.appendFile(process.argv[4], '\n');
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) console.log(err);
+  fs.readFile(process.argv[3], (error, content) => {
+    if (error) console.log(error);
+    fs.writeFile(process.argv[4], data + content, () => {});
+  });
+});
