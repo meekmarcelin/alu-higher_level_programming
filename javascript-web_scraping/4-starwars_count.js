0546fce36112faec606e9e920610b10832304109
meekmarcelin <meekmarcelin@gmail.com>
2022-10-11 07:21:22 -0700

    4 commit

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+require('request').get(process.argv[2], (err, response, body) => {
+  if (err) console.log(err);
+  console.log(JSON.parse(body).results.filter(el => el.characters.find(character => character.includes('18'))).length);
+});
