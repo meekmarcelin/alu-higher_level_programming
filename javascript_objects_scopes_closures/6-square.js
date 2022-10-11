8abfa0d30aa83d915a9d6751ce5344bb7246899f
meekmarcelin <meekmarcelin@gmail.com>
2022-10-11 01:14:28 -0700

    6 commit

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-module.exports = class square extends require('./5-square.js') { charPrint (c) { if (c === unidefined) { c = 'X'; } for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); } } };
+module.exports = class Square extends require('./5-square.js') { charPrint (c) { if (c === unidefined) { c = 'X'; } for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); } } };
