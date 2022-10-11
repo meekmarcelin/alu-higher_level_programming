fb8f8cd816fcdb501bf06394a6b544be8a417bc6
meekmarcelin <meekmarcelin@gmail.com>
2022-10-11 07:12:40 -0700

    3 commit

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, r, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+const request = require('request');
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) console.log(err);
+  console.log(JSON.parse(body).title);
 });
